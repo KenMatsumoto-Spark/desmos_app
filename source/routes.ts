8d9491469875809348e50c4e9d95a9424bf65126
Ken Matsumoto <h264matsumoto@gmail.com>
2025-06-10 17:46:29 -0300

    added get/ rerout

--- source/routes.ts
+++ source/routes.ts
@@ -4,6 +4,9 @@ import authentication from './middleware/authentication'
 
 const app = Router()
 
+app.get('/', function (req, res) {
+  res.redirect('/public');
+});
 
 app.use('/data', authentication, controllers.DataController)
 
